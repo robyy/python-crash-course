f9639f2dbcff29ef07996075728dd252f6100e35
yyan <yyan@aquent.com>
2020-02-23 15:51:15 -0500

    ship class

--- alien-invasion/core/alien_invasion.py
+++ alien-invasion/core/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from core.settings import Settings
+from core.ship import Ship
 
 
 class AlienInvasion:
@@ -16,6 +17,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        # Ship constructor needs an instance of AlienInvasion class
+        self.ship = Ship(self)
 
 
     def run_game(self):
@@ -32,6 +36,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
