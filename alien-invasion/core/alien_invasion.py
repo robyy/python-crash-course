b926f2c74a8d474902f20529954f474cf6358edc
yyan <yyan@aquent.com>
2020-02-23 15:08:30 -0500

    main class

---
+++ alien-invasion/core/alien_invasion.py
@@ -0,0 +1,45 @@
+import sys
+
+import pygame
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        # self.settings = Settings()
+
+        # self.screen = pygame.display.set_mode(
+        #     (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+
+            # To access the events that Pygame detects, we’ll use the pygame.event.get() function. This function returns a list of events
+            # that have taken place since the last time this function was called.
+            # Any keyboard or mouse event will cause this for loop to run.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
