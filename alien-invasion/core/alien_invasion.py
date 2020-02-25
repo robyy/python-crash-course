560b48fbf0660199c15dc463b7ef49b64407e998
yyan <yyan@aquent.com>
2020-02-25 09:56:14 -0500

    full screen mode

--- alien-invasion/core/alien_invasion.py
+++ alien-invasion/core/alien_invasion.py
@@ -16,6 +16,12 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+
+        # full screen mode
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
 
         # Ship constructor needs an instance of AlienInvasion class
