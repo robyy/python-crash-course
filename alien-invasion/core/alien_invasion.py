b0d08f47920a04176ca535310b0dd6ceb8d0e3ba
yyan <yyan@aquent.com>
2020-02-25 15:39:42 -0500

    aliens move to right

--- alien-invasion/core/alien_invasion.py
+++ alien-invasion/core/alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     # In Python, a single leading underscore indicates a helper method. Kinda a private method
@@ -94,6 +95,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
