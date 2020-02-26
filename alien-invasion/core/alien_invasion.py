25de50de7a597f8ee9c96913dbb7baa398cde369
yyan <yyan@aquent.com>
2020-02-26 10:04:06 -0500

    alien reaches bottom

--- alien-invasion/core/alien_invasion.py
+++ alien-invasion/core/alien_invasion.py
@@ -127,6 +127,9 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and find the number of aliens in a row.
@@ -197,6 +200,15 @@ class AlienInvasion:
         self.ship.center_ship()
         # Pause.
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
 
 if __name__ == '__main__':
