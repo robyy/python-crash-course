38470148b64074b8a05b88b72060cd361a31c083
yyan <yyan@aquent.com>
2020-03-06 14:46:31 -0500

    topic page

--- learning-log-project/learning_logs/urls.py
+++ learning-log-project/learning_logs/urls.py
@@ -7,9 +7,13 @@ from . import views
 # this defines a namespace, can be used in like template tag:
 # <a href="{% url 'learning_logs:index' %}">
 app_name = 'learning_logs'
+
 urlpatterns = [
     # Home page
-    # name can be used other places like in template:
-    # <a href="{% url 'learning_logs:index' %}">
+    # name can be used other places like in template: <a href="{% url 'learning_logs:index' %}">
     path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
