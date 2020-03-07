c11a3f07029b0f674f1878e77b25588be9196775
yyan <yyan@aquent.com>
2020-03-07 16:43:40 -0500

    boostrap

--- learning-log-project/config/settings.py
+++ learning-log-project/config/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # Third party apps.
+    'bootstrap4',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
