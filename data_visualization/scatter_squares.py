56c9949638f0c80b7a20474ad3077838fa5202e6
yyan <yyan@aquent.com>
2020-03-14 10:32:48 -0400

    automatically save

--- data_visualization/scatter_squares.py
+++ data_visualization/scatter_squares.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 
-
 x_values = range(1, 1001)
-y_values = [x**2 for x in x_values]
+y_values = [x ** 2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
@@ -16,8 +15,12 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-
 # Set the range for each axis.
 ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
+
+# If you want your program to automatically save the plot to a file,
+# you can replace the call to plt.show() with a call to plt.savefig():
+# The second argument trims extra whitespace from the plot. If you want the extra whitespace around the plot, just omit this argument.
+#plt.savefig('squares_plot.png', bbox_inches='tight')
