bf60f5136e7b381b537c4f5c1c6e43a0792fe318
yyan <yyan@aquent.com>
2020-03-14 10:07:46 -0400

    give input

--- data_visualization/mpl_square.py
+++ data_visualization/mpl_square.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 # matplotlib.pyplot.subplots() can generate one or more plots in the same figure.
 # The variable fig represents the entire figure or collection of plots that are generated.
 # The variable ax represents a single plot in the figure and is the variable we’ll use most of the time.
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
