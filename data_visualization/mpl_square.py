0e9ad6a9f1979b1703a29c24b1e95deea1fa27e8
yyan <yyan@aquent.com>
2020-03-14 10:04:02 -0400

    more readable

--- data_visualization/mpl_square.py
+++ data_visualization/mpl_square.py
@@ -5,6 +5,14 @@ squares = [1, 4, 9, 16, 25]
 # The variable fig represents the entire figure or collection of plots that are generated.
 # The variable ax represents a single plot in the figure and is the variable we’ll use most of the time.
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth=3)
+
+# Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
