858b3cd5e96637cc47efa76bca22dc26d3c00063
yyan <yyan@aquent.com>
2020-03-15 16:03:10 -0400

    list comprehension

--- data_visualization/dice_visual.py
+++ data_visualization/dice_visual.py
@@ -8,17 +8,17 @@ die_1 = Die()
 die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
-results = []
-for roll_num in range(50_000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for _ in range(50_000)]
+# for roll_num in range(50_000):
+#     result = die_1.roll() + die_2.roll()
+#     results.append(result)
 
 # Analyze the results.
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result+1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(2, max_result+1)]
+# for value in range(2, max_result+1):
+#     frequency = results.count(value)
+#     frequencies.append(frequency)
 
 # Visualize the results.
 x_values = list(range(2, max_result+1))
