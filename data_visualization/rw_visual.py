c070f40c4fc4b9cd55610c468726d597830a5b56
yyan <yyan@aquent.com>
2020-03-15 15:25:31 -0400

    fig size

--- data_visualization/rw_visual.py
+++ data_visualization/rw_visual.py
@@ -5,18 +5,19 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
+
     point_numbers = range(rw.num_points)
 
     # point_numbers is a list, passed to c parameter will result in a gradient of cmap clolors
     # based on the order of the dots is plotted(the earlier the dot is plotted the lighter)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-               edgecolors='none', s=15)
+               edgecolors='none', s=1)
 
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
