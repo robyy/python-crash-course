f19a52be12ee9a558b4fd83b03fc90fa598662f2
yyan <yyan@aquent.com>
2020-03-15 17:41:43 -0400

    low and high

--- data_visualization/sitka_highs.py
+++ data_visualization/sitka_highs.py
@@ -9,21 +9,24 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    # Get dates and high temperatures from this file.
-    dates, highs = [], []
+    # Get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-# Plot the high temperatures.
+# Plot the high and low temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 
 # Format plot.
-ax.set_title("Daily high temperatures - 2018", fontsize=24)
+ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
