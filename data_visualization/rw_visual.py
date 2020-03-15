da3afdf7d377db710ffc0a0f448748eb6487748a
yyan <yyan@aquent.com>
2020-03-15 15:22:01 -0400

    styling plat

--- data_visualization/rw_visual.py
+++ data_visualization/rw_visual.py
@@ -11,8 +11,22 @@ while True:
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
 
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    # point_numbers is a list, passed to c parameter will result in a gradient of cmap clolors
+    # based on the order of the dots is plotted(the earlier the dot is plotted the lighter)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+               edgecolors='none', s=15)
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+               s=100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
