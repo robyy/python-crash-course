a7a12d531c88193209c4c45e4b730c2a3a594daa
yyan <yyan@aquent.com>
2020-03-15 18:05:52 -0400

    death valley high-low plot

---
+++ data_visualization/death_valley_highs_lows.py
@@ -0,0 +1,45 @@
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+# Plot the high and low temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+# alpha controls color's transparency, 0 is completely transparent
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+# The facecolor argument determines the color of the shaded region;
+# we give it a low alpha value of 0.1 so the filled region connects the two data series without
+# distracting from the information they represent
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Format plot.
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
