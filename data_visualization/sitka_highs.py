34590737e5a073490e17617355ae4f0148060259
yyan <yyan@aquent.com>
2020-03-15 17:39:28 -0400

    year data

--- data_visualization/sitka_highs.py
+++ data_visualization/sitka_highs.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -23,7 +23,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
 # Format plot.
-ax.set_title("Daily high temperatures, July 2018", fontsize=24)
+ax.set_title("Daily high temperatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
