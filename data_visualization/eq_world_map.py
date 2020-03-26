328d75caf03dff9249c7031db4fac1c20e40c295
yyan <yyan@aquent.com>
2020-03-26 16:47:27 -0400

    scattergeo, colorscale

--- data_visualization/eq_world_map.py
+++ data_visualization/eq_world_map.py
@@ -2,9 +2,10 @@ import json
 
 from plotly import offline
 from plotly.graph_objs import Layout
+from plotly import colors
 
 # Explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -31,6 +32,10 @@ data = [{
     'lat': lats,
     'marker': {
         'size': [5 * mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
     },
 }]
 
@@ -38,3 +43,8 @@ my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
+
+# To see all the available colorscales
+# for key in colors.PLOTLY_SCALES.keys():
+#     print(key)
+
